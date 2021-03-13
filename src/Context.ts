98d27116e5e75db1cf38b08c955375faaa87bbe1
mr-exception <darbandi1996@gmail.com>
2021-03-13 14:12:46 +0330

    worked on configs

--- src/Context.ts
+++ src/Context.ts
@@ -4,7 +4,10 @@ import Sparow from "sparow-api";
 import { IProfile, emptyProfile } from "sparow-api/dist/interfaces/profile";
 
 export default createContext<IContext>({
-  sparow: new Sparow("http://localhost:5000/api", "ws://localhost:5003"),
+  sparow: new Sparow(
+    "http://core.sparow.salimon.ir/api",
+    "ws://salimon.ir:5003"
+  ),
   user: emptyProfile,
   authChanged: new Subject<void>(),
   unauthorize: () => {
--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,12 @@ import io from "socket.io-client";
 const echo = new Echo({
   broadcaster: "socket.io",
   client: io,
-  host: "localhost:6001",
+  host: "salimon.ir:5003",
   auth: {},
   transports: ["websocket"],
 });
 echo.channel("plain-message").listen("PlainMessage", (e: any) => {
-  console.log(e, "event");
+  console.log(e);
 });
 ReactDOM.render(
   <React.StrictMode>
